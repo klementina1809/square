5cb466fc8aaf4213bf4873203a535b2e36203793
klementina1809 <klementina.ua@gmail.com>
2024-03-15 16:10:18 +0100

    borders

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,86 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [tl, settl] = useState(0);
+	const [tr, settr] = useState(0);
+	const [bl, setbl] = useState(0);
+	const [br, setbr] = useState(0);
+	const [all, setall] = useState(0);
+
+	const handlerBorderRadius = (e) => {
+		const newValue = e.target.value;
+		if (e.target.name === "tl") settl(newValue);
+		else if (e.target.name === "tr") settr(newValue);
+		else if (e.target.name === "bl") setbl(newValue);
+		else if (e.target.name === "br") setbr(newValue);
+		else if (e.target.name === "all") {
+			setall(newValue);
+			settl(newValue);
+			settr(newValue);
+			setbl(newValue);
+			setbr(newValue);
+		}
+	};
+
+	useEffect(() => {
+		console.log("tl", tl);
+	}, [tl]);
+
+	const squareStyle = { borderRadius: `${tl}px ${tr}px ${br}px ${bl}px` };
 
 	return (
 		<>
-			<div className="square"></div>
+			<div className="square" style={squareStyle}></div>
+			<div>
+				<label>
+					All borders
+					<input
+						name="all"
+						type="number"
+						value={all}
+						onChange={(e) => handlerBorderRadius(e)}
+					/>
+				</label>
+			</div>
 
 			<div className="form">
-				<label htmlFor="">
+				<label>
 					Top Left
-					<input type="text" />
+					<input
+						name="tl"
+						type="number"
+						value={tl}
+						onChange={(e) => handlerBorderRadius(e)}
+					/>
 				</label>
 
-				<label htmlFor="">
+				<label>
 					Top Right
-					<input type="text" />
+					<input
+						name="tr"
+						type="number"
+						value={tr}
+						onChange={(e) => handlerBorderRadius(e)}
+					/>
 				</label>
-				<label htmlFor="">
+				<label>
 					Bottom Left
-					<input type="text" />
+					<input
+						name="bl"
+						type="number"
+						value={bl}
+						onChange={(e) => handlerBorderRadius(e)}
+					/>
 				</label>
-				<label htmlFor="">
+				<label>
 					Bottom Right
-					<input type="text" />
+					<input
+						name="br"
+						type="number"
+						value={br}
+						onChange={(e) => handlerBorderRadius(e)}
+					/>
 				</label>
 			</div>
 		</>
